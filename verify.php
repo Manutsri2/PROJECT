98b1445b1f8730dbfd349a62e7b8cbc5d5753d84
Manutsri2 <33697788+Manutsri2@users.noreply.github.com>
2017-11-16 00:35:57 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-    $access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+    $access_token = '7E/Ub3PcomIMFVemjLJKZJqTjiPo0LgEmKL3gybU+2i4JTe/rIDpOM21XcvHVfUCnfWS/nCsoaEdSbVpGL8J2yDmpXMmk4708xxB49wY/h2G6nMEQpPJHuMz5luKXg+g/p1LnRGQFoKX+mimkVLrsgdB04t89/1O/w1cDnyilFU=';
 
     $url = 'https://api.line.me/v1/oauth/verify';
 
