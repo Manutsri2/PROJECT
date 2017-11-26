19c5f21e75dea7bba892d5f65168788846655861
Manutsri2 <33697788+Manutsri2@users.noreply.github.com>
2017-11-26 23:39:06 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -20,14 +20,21 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			
+			if($text == "1"){
 			// Build message to reply back
 			$messages = [
 				
 				'type' => 'text',
 				'text' => 'JAYJUNG'
 				
-			];
+			];}
+			else{
+			$messages = [
+				
+				'type' => 'text',
+				'text' => 'WATCHARIT'
+				
+			];}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
