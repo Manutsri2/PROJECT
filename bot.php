dfc34f99432df00b43e8bf0868378abe9060b6bb
Manutsri2 <33697788+Manutsri2@users.noreply.github.com>
2017-11-26 23:32:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -35,7 +35,6 @@ if (!is_null($events['events'])) {
 				'text' => 'WATCHARIT'
 				
 			];}
-			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
