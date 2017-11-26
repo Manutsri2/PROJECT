f648fe9a907afa9ef553c2b4bef3bb2774477f0b
Manutsri2 <33697788+Manutsri2@users.noreply.github.com>
2017-11-26 23:21:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,14 +23,10 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				{
+				
 				'type' => 'text',
 				'text' => 'JAYJUNG'
-				},
-				{
-				'type' => 'text',
-				'text' => 'WATCHARIT'
-				}
+				
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
