1a228269c091cc48ccb74e5360bdc4aaf90e9ea0
Manutsri2 <33697788+Manutsri2@users.noreply.github.com>
2017-11-27 00:46:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -38,21 +38,21 @@ if (!is_null($events['events'])) {
     				'type' => 'text',
     				'text' => 'การแต่งกาย'
 				
-				];
-				/*
+				]
+				,
 				]
 				'type' => 'image',
     				'originalContentUrl' => 'https://www.picz.in.th/images/2017/11/27/jayjung1.jpg',
     				'previewImageUrl' => 'https://www.picz.in.th/images/2017/11/27/jayjung2.jpg'
 				
-				];*/
+				];
 			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$messages],[$messages]
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
